d686633ecccfe821a33ba6049e23ca6edfb75f82
kurita-t <mikanesan420@gmail.com>
2021-05-16 16:06:10 +0900

    予定追加ビュー作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_18_034941) do
+ActiveRecord::Schema.define(version: 2021_04_07_045427) do
+
+  create_table "plans", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "site_name"
+    t.string "place"
+    t.integer "period"
+    t.datetime "meeting_time"
+    t.string "number_of_people"
+    t.text "belongings"
+    t.integer "phone_number"
+    t.string "car"
+    t.integer "prefecture_id"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_plans_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
