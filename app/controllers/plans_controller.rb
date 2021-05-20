55f1cd406af1d44f0aa333c605a28d5d0f722804
kurita-t <mikanesan420@gmail.com>
2021-05-20 16:38:42 +0900

    予定追加機能追加(DB保存まで)

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,4 +1,26 @@
 class PlansController < ApplicationController
   def index
+    @plans = Plan.all
   end
+
+  def new
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.create(plan_params)
+    if @plan.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:site_name, :place, :period, :meeting_time, :number_of_people, :belongings, :phone_number, :car, :prefecture_id).merge(user_id: current_user.id)
+  end
+
 end
