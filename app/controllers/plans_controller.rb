8ac57caeee65be9127f19e867042f86a08f77e77
kurita-t <mikanesan420@gmail.com>
2021-05-30 14:40:33 +0900

    予定内容変更画面作成

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -21,6 +21,18 @@ class PlansController < ApplicationController
 
   end
 
+  def edit
+    @plan = Plan.find(params[:id])
+  end
+
+  def update
+    @plan = Plan.find(params[:id])
+    if @plan.update(plan_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
   private
 
   def plan_params
