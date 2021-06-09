c0d58d762e44e9cb59f6489f59261f2854c8779d
kurita-t <mikanesan420@gmail.com>
2021-06-09 20:45:11 +0900

    メッセージ機能作成(チャット機能)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,18 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @messages = Message.all
+    # @message = Message.find(params[:message_id])
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to messages_path
+    else
+      @messages = messages.includes(:user)
+      render :index
+    end
   end
 
   private
