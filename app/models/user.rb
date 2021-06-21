e387716d37e828809ed5700ed1d4cc4b41b21201
kurita-t <mikanesan420@gmail.com>
2021-06-21 14:20:55 +0900

    正規表現の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ApplicationRecord
          
   has_many :plans
   has_many :messages
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  with_options presence: true do
+    validates :nickname
+    validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birth_day
+  end
 end
